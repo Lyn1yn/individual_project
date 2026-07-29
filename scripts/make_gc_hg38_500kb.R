dc5afaf429101b1f4eb41ad7f9ae050deea1818a
Lyn1yn <lynlynyaha@gmail.com>
2026-07-29 13:33:17 +0100

    add commit for r script

--- scripts/make_gc_hg38_500kb.R
+++ scripts/make_gc_hg38_500kb.R
@@ -27,17 +27,17 @@ out_file <- file.path(out_dir, "g_hg38_500kb.wig")
 
 
 
-genome <- BSgenome.Hsapiens.UCSC.hg38
+genome <- BSgenome.Hsapiens.UCSC.hg38 #change genome object into a shorter name
 
-window <- 500000
-chromosomes <- paste0("chr", c(1:22))
+window <- 500000 #bin width of 500kb, matching the ichorCNA solution
+chromosomes <- paste0("chr", c(1:22)) #only select autosomes
 
 con <- file(out_file, open = "w")
 
 for (chr in chromosomes) {
-  seq_chr_full <- genome[[chr]]
-  chr_len <- length(seq_chr_full)
-  starts <- seq(1, chr_len, by = window)
+  seq_chr_full <- genome[[chr]] #get a DNAString
+  chr_len <- length(seq_chr_full) #get the length of chromosomes
+  starts <- seq(1, chr_len, by = window) #get the position of bin start
 
   cat(
     paste0(
@@ -47,24 +47,24 @@ for (chr in chromosomes) {
       "\n"
     ),
     file = con
-  )
+  ) #make a string, and put the string into a file
 
   for (start in starts) {
-    end <- min(start + window - 1, chr_len)
-    seq_bin <- subseq(seq_chr_full, start = start, end = end)
+    end <- min(start + window - 1, chr_len) #count the end of the bin
+    seq_bin <- subseq(seq_chr_full, start = start, end = end) #take out from start to end sequence in the whole chromosome
 
     counts <- letterFrequency(
       seq_bin,
-      letters = c("G", "C", "A", "T"),
+      letters = c("G", "C", "A", "T"), #count how many times letters show up in the sequence
       as.prob = FALSE
     )
 
-    total_acgt <- sum(counts)
+    total_acgt <- sum(counts) #only ATCG, no N 
 
     if (total_acgt == 0) {
       gc <- 0
     } else {
-      gc <- (counts["G"] + counts["C"]) / total_acgt
+      gc <- (counts["G"] + counts["C"]) / total_acgt #calculate GC percentage
     }
 
     cat(sprintf("%.6f\n", gc), file = con)
